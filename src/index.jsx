73e1cfbd1292462f0274bb5fb7b4061de68e014c
ngonidzashe utete <ngonieutete@gmail.com>
2022-10-20 12:05:16 +0200

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import { render } from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
